c1aef21c0321c209ea6fb658f7cb8fb9f21a98ff
Burak Bilgili <burak.bilgili@icloud.com>
2024-09-16 16:17:52 +0300

    add constants and props

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = 'TechPeople Blog';
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
+export const DESCRIPTION =
+  'Articles, stories and tutorials from tech enthusiasts.';
